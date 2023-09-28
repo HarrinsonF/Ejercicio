27a90d998d894cf1a95c4b760c6763d12e4e184f
Harrinson <hfloresbernuy@gmail.com>
2023-09-28 12:48:07 -0500

    Soltando Factos

---
+++ src/main/java/com/flores/examen/entitys/Saaverrosca.java
@@ -0,0 +1,20 @@
+package com.flores.examen.entitys;
+
+public class Saaverrosca {
+	private String Cabro;
+	private String Cabrazo;
+	public String getCabro() {
+		return Cabro;
+	}
+	public void setCabro(String cabro) {
+		Cabro = cabro;
+	}
+	public String getCabrazo() {
+		return Cabrazo;
+	}
+	public void setCabrazo(String cabrazo) {
+		Cabrazo = cabrazo;
+	}
+	
+	
+}
